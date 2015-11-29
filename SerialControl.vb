4bfe74b98b305095d0eb144a3ebdcbb711b81299
bruwesley <bruwesley@gmail.com>
2015-11-29 15:12:39 -0200

    SerialControl.vb
    SerialControl.vb

---
+++ SerialControl.vb
@@ -0,0 +1,14 @@
+Imports System.IO
+Imports System
+Imports System.Web
+Imports System.Threading
+Imports System.Net
+Imports System.Reflection
+Imports System.IO.Ports
+
+
+
+Module SerialControl
+
+ 
+End Module
